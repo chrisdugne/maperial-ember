1dddb576f7da7cf6b8a7d9a05175922c0ffeb715
Chris <chris.dugne@mapnify.com>
2012-11-21 20:35:04 +0100

    test ebean

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import com.avaje.ebean.Ebean;
 
 import models.User;
+import play.Logger;
+import play.db.ebean.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.main;
@@ -11,15 +13,22 @@ public class Application extends Controller
 {
 	// ---------------------------------------------//
 
+	@Transactional
 	public static Result home()
 	{
+		Logger.debug("---------------");
+		Logger.debug("new user");
 		User user = new User();
 		user.setEmail("chris.dugne@mapnify.com");
 		user.setGoogleUID("no");
 		user.setPassword("pwd");
 		user.setUserUID("777777");
+		Logger.debug("---------------");
+		Logger.debug("saving user");
 		
 		Ebean.save(user);
+		Logger.debug("---------------");
+		Logger.debug("rendering");
 		
 		return ok(main.render());
 	}
