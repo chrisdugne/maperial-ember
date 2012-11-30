519305fe58ca35d8fe5b113f8cd6ee3d5c5a2bf0
Chris <chris.dugne@mapnify.com>
2012-11-30 17:50:18 +0100

    premier item pour dashboard

--- public/javascripts/ember/views/dashboard.js
+++ public/javascripts/ember/views/dashboard.js
@@ -2,9 +2,27 @@
 	'use strict';
 
 	var DashboardView = Ember.View.extend({
-		templateName: 'dashboard'
+		templateName: 'dashboard',
+		didInsertElement: function(){
+			renderDashboardUI();
+		},
+		willDestroyElement: function(){
+			cleanDashboardUI();
+		}
 	});
 	
 	app.DashboardView = DashboardView;
 
 })( window.Webapp);
+
+//--------------------------------------------------------------------------//
+
+function renderDashboardUI()
+{
+	
+}
+
+function cleanDashboardUI()
+{
+	
+}
