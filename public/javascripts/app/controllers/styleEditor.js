3dc320ec768bd30c993921e8ca959da8e0d129d4
Chris <chris.dugne@mapnify.com>
2013-01-04 13:31:50 +0100

    styleEditor : map + popups

--- public/javascripts/app/controllers/styleEditor.js
+++ public/javascripts/app/controllers/styleEditor.js
@@ -8,21 +8,79 @@
 
 	StyleEditorController.renderUI = function()
 	{
-
+		ScriptLoader.getScripts(["http://map.x-ray.fr/js/gl-matrix-min.js",
+		                         "http://map.x-ray.fr/js/gl-tools.js",
+		                         "http://map.x-ray.fr/js/coordinate-system.js",
+		                         "http://map.x-ray.fr/js/jquery.mousewheel.min.js",
+		                         "http://map.x-ray.fr/js/render-line.js",
+		                         "http://map.x-ray.fr/js/render-text.js",
+		                         "http://map.x-ray.fr/js/tileRenderer.js",
+		                         "http://map.x-ray.fr/js/maps.js"],
+                 function()
+                 {
+					StyleEditorController.renderStyle();
+					StyleEditorController.renderOptions();
+      				
+					StyleEditorController.renderMap();
+      				$(".popup").dialogr().parents('.ui-dialog').draggable('option', 'snap', true);
+                 }
+      	);
 	}
 
 	StyleEditorController.cleanUI = function()
 	{
-		
+		StyleEditorController.cleanStyle();
+		StyleEditorController.cleanOptions();
+		StyleEditorController.cleanMap();
 	}
 
 	//------------------------------------------------//
 
-	StyleEditorController.save = function() 
+	StyleEditorController.renderMap = function()
 	{
-		
+		var maps = new GLMap ( "map" );
+		maps.Start();
+
+		$("#map").css("height", $("#webappDiv").height() );
+		$("#map").css("width", $("#webappDiv").width() );
 	}
 
+	StyleEditorController.cleanMap = function()
+	{
+		$("#map").remove();
+	}
+	
+	//------------------------------------------------//
+
+	StyleEditorController.renderStyle = function()
+	{
+		$("#style").dialogr({
+			position : [80,410],
+			closeOnEscape: false,
+			dialogClass: 'no-close'
+		});
+	}
+
+	StyleEditorController.cleanStyle = function()
+	{
+		$("#style").remove();
+	}
+	//------------------------------------------------//
+	
+	StyleEditorController.renderOptions = function()
+	{
+		$("#options").dialogr({
+			position : [80,110],
+			closeOnEscape: false,
+			dialogClass: 'no-close'
+		});
+	}
+	
+	StyleEditorController.cleanOptions = function()
+	{
+		$("#options").remove();
+	}
+	
 	//------------------------------------------------//
 
 	app.StyleEditorController = StyleEditorController;
--- public/javascripts/app/controllers/tryscreen.js
+++ public/javascripts/app/controllers/tryscreen.js
@@ -18,11 +18,11 @@
 		                         "http://map.x-ray.fr/js/maps.js"],
                  function()
                  {
-      				renderColorBar();
-      				renderStyle();
-      				initColorPicker();
+					TryscreenController.renderColorBar();
+					TryscreenController.renderStyle();
+					TryscreenController.initColorPicker();
       				
-      				renderMap();
+					TryscreenController.renderMap();
       				$(".popup").dialogr().parents('.ui-dialog').draggable('option', 'snap', true);
                  }
       	);
@@ -30,13 +30,105 @@
 
 	TryscreenController.cleanUI = function()
 	{
-		cleanColorBar();
-		cleanStyle();
-		cleanColorPicker();
-		cleanMap();
+		TryscreenController.cleanColorBar();
+		TryscreenController.cleanStyle();
+		TryscreenController.cleanColorPicker();
+		TryscreenController.cleanMap();
 	}
 
 	//------------------------------------------------//
+
+
+	TryscreenController.renderColorBar = function()
+	{
+		$("#colorbar").dialogr({
+			position : [80,110],
+			closeOnEscape: false,
+			dialogClass: 'no-close'
+		});
+		
+	}
+
+	TryscreenController.cleanColorBar = function()
+	{
+		$("#colorbar").remove();
+	}
+	
+	//------------------------------------------------//
+
+
+	TryscreenController.initColorPicker = function()
+	{
+		$("#colorpicker").dialogr({
+			position : [380,210],
+			closeOnEscape: false,
+			dialogClass: 'no-close',
+			autoOpen: false,
+			show: {effect: 'fade', duration: 250},
+			hide: {effect: 'fade', duration: 250}
+		});
+	}
+
+	TryscreenController.cleanColorPicker = function()
+	{
+		$("#colorpicker").remove();
+	}
+
+
+	TryscreenController.openColorPicker = function(div)
+	{
+		var position = $("#"+div.id).offset();
+		var left = position.left;
+		var top = position.top;
+
+		$("#colorpicker").html("<div id=\"picker\"></div>")
+						 .dialogr('option', 'title', 'Color')
+						 .dialogr('option', 'width', 300)
+						 .dialogr('option', 'height', 380)
+						 .dialogr('option', 'position', [left, top])
+						 .dialogr("open");
+
+		$("#picker").colorpicker({color:rgb2hex(div.style.backgroundColor)})
+		.on('change.color', function(evt, color){
+			$('#'+div.id).attr('style','background-color:'+color);
+			$("#colorpicker").dialogr("close");
+		});
+	}
+	
+	//------------------------------------------------//
+
+	TryscreenController.renderMap = function()
+	{
+		var maps = new GLMap ( "map" );
+		maps.Start();
+
+		$("#map").css("height", $("#webappDiv").height() );
+		$("#map").css("width", $("#webappDiv").width() );
+	}
+
+	TryscreenController.cleanMap = function()
+	{
+		$("#map").remove();
+	}
+	
+	//------------------------------------------------//
+
+	TryscreenController.renderStyle = function()
+	{
+		$("#style").dialogr({
+			position : [80,410],
+			closeOnEscape: false,
+			dialogClass: 'no-close'
+		});
+	}
+
+	TryscreenController.cleanStyle = function()
+	{
+		$("#style").remove();
+	}
+	
+	//------------------------------------------------//
+	
 	
 	app.TryscreenController = TryscreenController;
 
